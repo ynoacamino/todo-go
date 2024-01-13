9587462b9becdfdfb1f760c64ebb1292902526f8
Diegoo11 <ynoacamino@gmail.com>
2024-01-13 18:21:32 -0500

    init project

---
+++ client/.eslintrc.cjs
@@ -0,0 +1,28 @@
+module.exports = {
+  root: true,
+  env: { browser: true, es2020: true },
+  extends: [
+    'airbnb',
+    'airbnb-typescript',
+    'airbnb/hooks',
+    'plugin:@typescript-eslint/recommended-type-checked',
+    'plugin:@typescript-eslint/stylistic-type-checked',
+  ],
+  ignorePatterns: ['dist', '.eslintrc.cjs'],
+  parser: '@typescript-eslint/parser',
+  plugins: ['react-refresh'],
+  rules: {
+    'react-refresh/only-export-components': [
+      'warn',
+      { allowConstantExport: true },
+    ],
+    'react/react-in-jsx-scope': 0,
+    'react/function-component-definition':0,
+    'linebreak-style': 0,
+    'no-console': 0,
+    'jsx-a11y/label-has-associated-control': 0
+  },
+  parserOptions: {
+    project: './tsconfig.json'
+  }
+}
---
+++ client/src/App.tsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="bg-slate-100 min-h-screen w-full">
+      hola
+    </div>
+  );
+}
+
+export default App;
